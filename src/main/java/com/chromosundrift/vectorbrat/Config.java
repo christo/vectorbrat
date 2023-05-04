14eeb45425795bca0888bc6b09c27df230ae4e87
Chris Mountford <chris@think.net.au>
2023-05-05 00:25:20 +1000

    move config into its own class

---
+++ src/main/java/com/chromosundrift/vectorbrat/Config.java
@@ -0,0 +1,18 @@
+package com.chromosundrift.vectorbrat;
+
+public class Config {
+    public static final int SAMPLE_RATE = 96000;
+    public static final int CHANNEL_X = 9;
+    public static final int CHANNEL_Y = 10;
+    public static final int CHANNEL_R = 11;
+    public static final int CHANNEL_G = 12;
+    public static final int CHANNEL_B = 13;
+
+    /**
+     * The main ES-9 device
+     */
+    public static final String ES9 = "ES-9";
+    public static final String deviceXY = "es-9_X_Y";
+    public static final String deviceRZ = "es-9_R_Z";
+    public static final String deviceGB = "es-9_G_B";
+}
