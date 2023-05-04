47ff988b03ce205b562c5832ed784d513feb18d9
Chris Mountford <chris@think.net.au>
2023-05-05 00:26:38 +1000

    build display interface and double-buffered model intermediary

---
+++ src/main/java/com/chromosundrift/vectorbrat/DoubleBufferedVectorDisplay.java
@@ -0,0 +1,58 @@
+package com.chromosundrift.vectorbrat;
+
+import com.chromosundrift.vectorbrat.geom.Model;
+
+import javax.swing.JPanel;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Function;
+
+/**
+ * Threadsafe, double-buffered display. Call {@link DoubleBufferedVectorDisplay#withLockAndFlip(Function)} to render
+ * the current model.
+ */
+public final class DoubleBufferedVectorDisplay implements VectorDisplay {
+    private final ReentrantLock lock = new ReentrantLock();
+    private Model frontModel = new Model();
+    private Model backModel = new Model();
+
+    /**
+     * Applies the function to the model using double-buffering and thread safety.
+     */
+    public void withLockAndFlip(Function<Model, Void> withLockAndFlip) {
+        try {
+            lock.lock();
+            withLockAndFlip.apply(frontModel);
+            flip();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * Sets the back buffer model only. Next render, this will be flipped to the front model.
+     * @param model the new model.
+     * @return this.
+     */
+    @Override
+    public VectorDisplay setModel(Model model) {
+        try {
+            lock.lock();
+            this.backModel = model;
+        } finally {
+            lock.unlock();
+        }
+        return this;
+    }
+
+    /**
+     * Flips the back model to the front.
+     */
+    public void flip() {
+        try {
+            lock.lock();
+            this.frontModel = this.backModel;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
--- src/main/java/com/chromosundrift/vectorbrat/VectorDisplay.java
+++ src/main/java/com/chromosundrift/vectorbrat/VectorDisplay.java
@@ -1,41 +1,7 @@
 package com.chromosundrift.vectorbrat;
 
-import javax.swing.BorderFactory;
-import javax.swing.JPanel;
-import javax.swing.border.TitledBorder;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
+import com.chromosundrift.vectorbrat.geom.Model;
 
-public class VectorDisplay extends JPanel {
-
-    private final Color colText = Color.PINK;
-    private final Color colBg = Color.DARK_GRAY.darker();
-    private final int inset = 50;
-
-    public VectorDisplay(String myTitle) {
-        super(true);
-        setBackground(Color.BLACK);
-        setForeground(Color.GREEN);
-
-    }
-
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        //setBackground(Color.GREEN);
-        final Dimension s = getSize();
-
-        BufferedImage im = new BufferedImage(s.width - inset*2, s.height - inset*2, BufferedImage.TYPE_INT_ARGB);
-        final Graphics2D g2 = (Graphics2D) im.getGraphics();
-
-        g2.setBackground(colBg);
-        g2.setColor(colText);
-        g2.clearRect(0, 0, im.getWidth(), im.getHeight());
-
-        g2.drawString("YO DOG", im.getWidth() / 2, im.getHeight() / 2);
-        g.drawImage(im, inset, inset, null);
-    }
+public interface VectorDisplay {
+    VectorDisplay setModel(Model model);
 }
