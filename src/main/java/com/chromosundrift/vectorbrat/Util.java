6cfc579783643aa4b70b6379497ef80e69d9c414
Chris Mountford <chris@think.net.au>
2023-05-07 19:04:22 +1000

    added logging to util

--- src/main/java/com/chromosundrift/vectorbrat/Util.java
+++ src/main/java/com/chromosundrift/vectorbrat/Util.java
@@ -1,11 +1,18 @@
 package com.chromosundrift.vectorbrat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Util {
+
+    private static final Logger logger = LoggerFactory.getLogger(Util.class);
+
     /**
      * Needed for jna to load jack native libraries, must be called before jack initialisation. Depends on
      * libjack.dylib existing at the specified path, can be installed on macos with <code>brew install jack</code>
      */
     public static void setSystemLibraryPath() {
+        logger.info("setting up native library paths");
         System.setProperty("jna.library.path", "/opt/homebrew/lib");
     }
 }
