796627dbe8bfe7b5fe7d7f404c6eefff25cfe603
Chris Mountford <chris@think.net.au>
2023-05-07 12:25:56 +1000

    tweaks

--- src/main/java/com/chromosundrift/vectorbrat/audio/Format.java
+++ src/main/java/com/chromosundrift/vectorbrat/audio/Format.java
@@ -1,6 +1,5 @@
 package com.chromosundrift.vectorbrat.audio;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,21 +13,20 @@ public final class Format {
 
     public enum Endianness {BIG, LITTLE}
 
-    private static final float MAX_SAMPLE_RATE = 96000;
-    private static final int MAX_CHANNELS = 16;
-    public static final List<Integer> LEGIT_BITS_ = Arrays.asList(8, 16, 24, 32);
+    public static final float MAX_SAMPLE_RATE = 96000;
+    public static final int MAX_CHANNELS = 16;
+    public static final List<Integer> LEGIT_BITS = Arrays.asList(8, 16, 24, 32);
 
     private final float rate;
     private final Endianness endianness;
     private final Signitude signitude;
     private final int channels;
 
-
     public Format(float sampleRate, int bits, Endianness endianness, Signitude signitude, int channels) {
         if (sampleRate < 1 || sampleRate > MAX_SAMPLE_RATE) {
             throw new IllegalArgumentException("sample rate out of range");
         }
-        if (!LEGIT_BITS_.contains(bits)) {
+        if (!LEGIT_BITS.contains(bits)) {
             throw new IllegalArgumentException("bits not legit");
         }
         if (channels < 1 || channels > MAX_CHANNELS) {
