81dd6c286bbcb0c96edfa8ae6b5c742f7ad24bd6
Chris Mountford <chris@think.net.au>
2023-05-08 21:04:55 +1000

    created shutdown callback

---
+++ src/main/java/com/chromosundrift/vectorbrat/audio/jack/JackShutdown.java
@@ -0,0 +1,14 @@
+package com.chromosundrift.vectorbrat.audio.jack;
+
+import org.jaudiolibs.jnajack.JackClient;
+import org.jaudiolibs.jnajack.JackShutdownCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JackShutdown implements JackShutdownCallback {
+    private static final Logger logger = LoggerFactory.getLogger(JackShutdown.class);
+    @Override
+    public void clientShutdown(JackClient client) {
+        logger.info("Jack shutdown initiated for client " +client.getName());
+    }
+}
