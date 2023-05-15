7fb95f44d3b29c91360b204868adc20f6aeda583
Chris Mountford <chris@think.net.au>
2023-05-15 10:13:18 +1000

    javadoc

--- src/main/java/com/chromosundrift/vectorbrat/swing/LaserController.java
+++ src/main/java/com/chromosundrift/vectorbrat/swing/LaserController.java
@@ -1,21 +1,48 @@
 package com.chromosundrift.vectorbrat.swing;
 
 public interface LaserController {
+
+    /**
+     * Is the laser on or off?
+     *
+     * @return true iff the laser is on.
+     */
     boolean getOn();
 
+    /**
+     * Turn the laser on or off.
+     *
+     * @param on if true, turns laser on, otherwise turns it off.
+     */
     void setOn(boolean on);
 
+    /**
+     * Gets the current points per second speed.
+     *
+     * @return the current value.
+     */
     int getPps();
 
+    /**
+     * Sets the current points per second laser speed. Actual setting is constraint between configured maximum and
+     * minimum values.
+     *
+     * @param fps the value.
+     */
     void setPps(int fps);
 
     /**
-     * Negative number means unknown / not applicable.
+     * Gets the current sample rate in Hz. Negative number means unknown / not applicable.
+     *
+     * @return the sample rate.
      */
     float getSampleRate();
 
     /**
-     * Negative number means unknown / not applicable.
+     * Gets the current buffer size. Can be updated by the driver implementation. Negative number means
+     * unknown / not applicable.
+     *
+     * @return the number of samples in the buffer.
      */
     int getBufferSize();
 }
