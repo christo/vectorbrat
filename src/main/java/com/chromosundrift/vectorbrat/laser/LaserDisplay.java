d2362a066a060140c8808169fbe40ee91e75a390
Chris Mountford <chris@think.net.au>
2023-05-18 10:04:17 +1000

    cleanup

--- src/main/java/com/chromosundrift/vectorbrat/laser/LaserDisplay.java
+++ src/main/java/com/chromosundrift/vectorbrat/laser/LaserDisplay.java
@@ -14,7 +14,6 @@ import com.chromosundrift.vectorbrat.Config;
 import com.chromosundrift.vectorbrat.DoubleBufferedVectorDisplay;
 import com.chromosundrift.vectorbrat.VectorBratException;
 import com.chromosundrift.vectorbrat.VectorDisplay;
-import com.chromosundrift.vectorbrat.geom.GlobalModel;
 import com.chromosundrift.vectorbrat.geom.Model;
 import com.chromosundrift.vectorbrat.geom.PathPlanner;
 
