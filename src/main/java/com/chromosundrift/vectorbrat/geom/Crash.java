224e548ad8ed9c857029221357c2cb317c6e5bc3
Chris Mountford <chris@think.net.au>
2023-06-08 18:03:49 +1000

    added part of the message text to a crash animator

--- src/main/java/com/chromosundrift/vectorbrat/geom/Crash.java
+++ src/main/java/com/chromosundrift/vectorbrat/geom/Crash.java
@@ -3,7 +3,16 @@ package com.chromosundrift.vectorbrat.geom;
 import java.util.Arrays;
 import java.util.Collections;
 
+import com.chromosundrift.vectorbrat.Util;
+
 public class Crash extends StaticAnimator {
+
+    /**
+     * Only uses small, fixed number of chars for now until line splitting is implemented and more efficient text is
+     * feasible.
+     */
+    private static final int MAX_TEXT_LEN = 16;
+
     public Crash(String message) {
         super("crash", redX(message));
     }
@@ -13,6 +22,8 @@ public class Crash extends StaticAnimator {
         Point.PointFactory red = new Point.PointFactory(Rgb.RED);
         Polyline bs = Polyline.open("\\", Rgb.RED, red.p(-0.1f, -0.1f), red.p(0.1f, 0.1f));
         Polyline fs = Polyline.open("/", Rgb.RED, red.p(0.1f, -0.1f), red.p(-0.1f, 0.1f));
-        return new Model("crash", Arrays.asList(bs, fs), Collections.emptyList());
+        TextEngine te = new TextEngine(Rgb.ORANGE, AsteroidsFont.INSTANCE);
+        Model textModel = te.textLine(Util.truncate(message, MAX_TEXT_LEN));
+        return new Model("crash", Arrays.asList(bs, fs), Collections.emptyList()).merge(textModel);
     }
 }
