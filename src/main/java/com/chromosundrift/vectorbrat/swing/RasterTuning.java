06fa22e0166419b4d6ac05bcdeb70f5919c6517f
Chris Mountford <chris@think.net.au>
2023-06-13 10:33:49 +1000

    added some ideas for what belongs in RasterTuning

--- src/main/java/com/chromosundrift/vectorbrat/swing/RasterTuning.java
+++ src/main/java/com/chromosundrift/vectorbrat/swing/RasterTuning.java
@@ -2,4 +2,8 @@ package com.chromosundrift.vectorbrat.swing;
 
 public class RasterTuning {
     // TODO figure out what this needs
+    // stroke width
+    // stroke transparency
+    // debug path color RGBA
+    // debag path stroke
 }
