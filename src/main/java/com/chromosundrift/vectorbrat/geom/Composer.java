0bfae30d50ccf13574fd22aa6bef4fef603b25b6
Chris Mountford <chris@think.net.au>
2023-09-15 10:45:38 +1000

    format

--- src/main/java/com/chromosundrift/vectorbrat/geom/Composer.java
+++ src/main/java/com/chromosundrift/vectorbrat/geom/Composer.java
@@ -5,9 +5,10 @@ import java.util.List;
 import com.chromosundrift.vectorbrat.VectorBratException;
 
 /**
- * Implementation of {@link ModelAnimator} that composes a list of animators together by merging the models.
- * Lifecycle methods start, stop and update are called in list order without exception handling. Any component methods
- * that throw exceptions will result in an incomplete composite call graph and the caller will get the exception.
+ * {@link ModelAnimator} that composes a list of animators together by merging the models.
+ * Lifecycle methods start, stop and update are called in list order without exception
+ * handling. Any component methods that throw exceptions will result in an incomplete
+ * composite call graph and the caller will get the exception.
  */
 public class Composer implements ModelAnimator {
 
