57ca2f5641361038bc28ff80793a6c5c04856598
Chris Mountford <chris@think.net.au>
2023-09-16 19:56:36 +1000

    added a ZERO for something that got deleted, keeping

--- src/main/java/com/chromosundrift/vectorbrat/geom/Vec2.java
+++ src/main/java/com/chromosundrift/vectorbrat/geom/Vec2.java
@@ -4,6 +4,8 @@ package com.chromosundrift.vectorbrat.geom;
  * 2D float vector.
  **/
 public record Vec2(float x, float y) {
+    public static final Vec2 ZERO = new Vec2(0f, 0f);
+
     public Vec2(Point p) {
         this(p.x(), p.y());
     }
