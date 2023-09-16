9f54396b0c7681a2c1772d8483221e1521b7c76d
Chris Mountford <chris@think.net.au>
2023-09-16 20:21:46 +1000

    implement default closest() in Pointless

--- src/main/java/com/chromosundrift/vectorbrat/geom/Pointless.java
+++ src/main/java/com/chromosundrift/vectorbrat/geom/Pointless.java
@@ -1,5 +1,6 @@
 package com.chromosundrift.vectorbrat.geom;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
@@ -8,9 +9,24 @@ import java.util.stream.Stream;
 abstract public class Pointless implements Geom {
     /**
      * Has no isopoints.
+     *
      * @return empty stream.
      */
     public final Stream<Point> isoPoints() {
         return Stream.empty();
+    }
+
+    /**
+     * Convenience implementation that compares all points of all lines. This won't be the most efficient
+     * for subclasses who know which points are shared in lines but it should be correct. Override if you
+     * know better.
+     *
+     * @param other comparison point.
+     * @return the closest point to other.
+     */
+    @Override
+    public Optional<Point> closest(Point other) {
+        // we know line always has a closest and if we are empty, min returns Empty
+        return lines().map(line -> line.closest(other).get()).min(other.dist2Point());
     }
 }
