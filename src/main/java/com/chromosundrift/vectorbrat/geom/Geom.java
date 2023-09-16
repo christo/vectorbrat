f1f33e06e7058ef928e9552bb526dea6c0bf5492
Chris Mountford <chris@think.net.au>
2023-09-16 20:21:59 +1000

    typo

--- src/main/java/com/chromosundrift/vectorbrat/geom/Geom.java
+++ src/main/java/com/chromosundrift/vectorbrat/geom/Geom.java
@@ -12,7 +12,7 @@ public interface Geom {
      * If the geometry is empty, return {@link Optional#empty()}.
      *
      * @param other comparison point.
-     * @return the closeest point to other in our geometry or if empty, return other.
+     * @return the closest point to other in our geometry or if empty, return other.
      */
     Optional<Point> closest(Point other);
 
