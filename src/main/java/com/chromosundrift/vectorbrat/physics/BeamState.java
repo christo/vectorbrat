e873d341333a04924d0af6343a0b0de35c2e0b22
Chris Mountford <chris@think.net.au>
2023-09-28 09:50:48 +1000

    added colour to BeamState

--- src/main/java/com/chromosundrift/vectorbrat/physics/BeamState.java
+++ src/main/java/com/chromosundrift/vectorbrat/physics/BeamState.java
@@ -1,9 +1,10 @@
 package com.chromosundrift.vectorbrat.physics;
 
+import com.chromosundrift.vectorbrat.geom.Rgb;
+
 /**
  * Mutable struct for physical state of hardware for a scanning laser, includes x and y velocity which are used to
  * determine impulse calculations.
- * future: will need to include colour state
  */
 final class BeamState {
 
@@ -11,18 +12,20 @@ final class BeamState {
     float yPos;
     float xVel;
     float yVel;
+    private final Rgb rgb;
 
-    public BeamState(float xPos, float yPos, float xVel, float yVel) {
+    public BeamState(float xPos, float yPos, float xVel, float yVel, Rgb rgb) {
         this.xPos = xPos;
         this.yPos = yPos;
         this.xVel = xVel;
         this.yVel = yVel;
+        this.rgb = rgb;
     }
 
     /**
-     * Zero-position state.
+     * Motionless origin state, white beam.
      */
     public BeamState() {
-        this(0f, 0f, 0f, 0f);
+        this(0f, 0f, 0f, 0f, Rgb.WHITE);
     }
 }
